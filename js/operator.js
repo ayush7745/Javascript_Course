29403d816350e0611733d0a16561e596e992dd80
ayush7745 <bhatnagara068@gmail.com>
2025-04-16 10:23:37 +0530

    Operators in JS - assignment opt

--- js/operator.js
+++ js/operator.js
@@ -15,3 +15,10 @@
 // console.log("a/b=",a/b);
 // console.log("a%b=",a%b);//modulus means remainder
 // console.log("a**b=",a**b);//exponentiation means 7 to the power 2 =49
+
+//Assignment operators
+let a=7;
+let b=2;
+a+=4;//a=a+4
+console.log("a=",a);
+//-= and *= are also used as given above
