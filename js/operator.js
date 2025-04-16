a81c37f9128f47f2a88a3600d6d09d01a00e84bd
ayush7745 <bhatnagara068@gmail.com>
2025-04-16 10:07:19 +0530

    Operators in JS - Arithmetic operator

---
+++ js/operator.js
@@ -0,0 +1,17 @@
+/*const product={
+    title:"ball pen",
+    rating:4.2,
+    price: 230,
+};*/
+
+//operators in JS
+//arithmetic operators is given below
+let a=7;
+let b=2;
+console.log("a=",a, "&b=",b);
+console.log("a-b=",a-b);
+console.log("a+b=",a+b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);//modulus means remainder
+console.log("a**b=",a**b);//exponentiation means 7 to the power 2 =49
