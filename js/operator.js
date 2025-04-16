15613629a562fa1803220db42e413c5212f9c634
ayush7745 <bhatnagara068@gmail.com>
2025-04-16 10:19:01 +0530

    Commented code

--- js/operator.js
+++ js/operator.js
@@ -6,12 +6,12 @@
 
 //operators in JS
 //arithmetic operators is given below
-let a=7;
-let b=2;
-console.log("a=",a, "&b=",b);
-console.log("a-b=",a-b);
-console.log("a+b=",a+b);
-console.log("a*b=",a*b);
-console.log("a/b=",a/b);
-console.log("a%b=",a%b);//modulus means remainder
-console.log("a**b=",a**b);//exponentiation means 7 to the power 2 =49
+// let a=7;
+// let b=2;
+// console.log("a=",a, "&b=",b);
+// console.log("a-b=",a-b);
+// console.log("a+b=",a+b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);//modulus means remainder
+// console.log("a**b=",a**b);//exponentiation means 7 to the power 2 =49
