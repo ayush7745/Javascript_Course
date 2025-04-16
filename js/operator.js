d80ba9957c550b085a4e069cad48bb7f67d6c586
ayush7745 <bhatnagara068@gmail.com>
2025-04-16 10:32:46 +0530

    Operators in JS - comparision operator

--- js/operator.js
+++ js/operator.js
@@ -22,3 +22,26 @@
 // a+=4;//a=a+4
 // console.log("a=",a);
 //-= and *= are also used as given above
+
+//Comparison operators
+// eequal to ==
+
+let a=2;
+let b=2;
+console.log("2==2",a==b);    //true
+
+ a=5;
+ b=6;
+ console.log ("5==6",a==b); //false
+
+ //not equal to !=
+
+ a=5;
+ b=6;
+ console.log ("5!=6",a!=b); //true
+
+ a="5";//string
+ b=5;//number
+ console.log("5==5",a==b); //true //this shows that if we compare any no.of different datatype,we get result on the base of data only
+
+ //equal to and datatype ===
